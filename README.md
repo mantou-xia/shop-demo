6d05352ac2146762380a1b4cca56c1feaa0ee6b6
mantouxia <3775060926@qq.com>
2025-03-20 18:17:23 +0800

    add 框架

--- README.md
+++ README.md
@@ -24,6 +24,26 @@ npm run lint
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
 
-
-
+# 拉取项目
+每次更改项目后，需要拉取项目，然后提交到github上
+```bash
 git clone git@github.com:mantou-xia/shop-demo.git
+
+git pull
+
+git add .
+
+git commit -m "注释"
+
+git pull
+```
+# 该项目插入了element-ui、vant、axios、vue-router、naive-ui、pinia、naive-ui
+
+# 项目核心功能
+## 登录/注册表单
+
+## 商品列表
+
+
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <div>nihao</div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <script>
 
 </script>
 
+<!-- 全局样式 -->
 <style>
+/* 全局样式需要单独声明 */
+body,
+html {
+  height: 100%;
+  margin: 0;
+}
+</style>
+<style scoped lang="scss">
 
+.el-header,
+.el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body>.el-container {
+  margin-bottom: 40px;
+}
 </style>
